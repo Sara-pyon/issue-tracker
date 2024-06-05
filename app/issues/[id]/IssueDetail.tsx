24b15d8e184c293d74878d837839fb7998bcf935
Sara Komatsu <radwimps.sara@gmail.com>
2024-06-05 15:12:44 -0500

    Refactor: Apply the SRP

---
+++ app/issues/[id]/IssueDetail.tsx
@@ -0,0 +1,21 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Card, Flex, Heading } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+
+const IssueDetail = ({ issue }: { issue: Issue }) => {
+  return (
+    <>
+      <Heading mb="2">{issue.title}</Heading>
+      <Flex gapX="2" align="center">
+        <IssueStatusBadge status={issue.status} />
+        <p>{issue.createdAt.toDateString()}</p>
+      </Flex>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetail;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,8 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import prisma from '@/prisma/client'
-import { Box, Button, Card, Flex, Grid, Heading } from '@radix-ui/themes';
+import prisma from '@/prisma/client';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import ReactMarkdown from 'react-markdown'
-import React from 'react'
-import Link from 'next/link';
-import { Pencil2Icon } from '@radix-ui/react-icons'
+import EditIssueButton from './EditIssueButton';
+import IssueDetail from './IssueDetail';
 
 interface Props{
     params: { id: string }
@@ -21,22 +18,10 @@ const IssueDetailPage = async({params}: Props) => {
   return (
     <Grid columns={{initial: "1", sm: "2"}} gap="5">
       <Box>
-        <Heading mb="2">{issue.title}</Heading>
-        <Flex gapX="2" align="center">
-          <IssueStatusBadge status={issue.status}/>
-          <p>{issue.createdAt.toDateString()}</p>
-        </Flex>
-        <Card className='prose' mt="4" >
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+       <IssueDetail issue={issue} />
       </Box>
       <Box>
-        <Link href={`/issues/${issue.id}/edit`}>
-          <Button variant='classic'>
-            <Pencil2Icon />
-              Edit
-          </Button>
-        </Link>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   )
