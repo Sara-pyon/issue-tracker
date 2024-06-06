459c58790a43d7d3fc9c6c1964e18d470def0ee2
Sara Komatsu <radwimps.sara@gmail.com>
2024-06-06 15:39:25 -0500

    Fetchng data with reactQuery

--- app/issues/[id]/AssingneeSelect.tsx
+++ app/issues/[id]/AssingneeSelect.tsx
@@ -1,26 +1,41 @@
-"use server";
+"use client";
 
-import prisma from '@/prisma/client'
-import { Select } from '@radix-ui/themes'
+import { User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Skelton from "../../components/Skelton";
 
-const AssingneeSelect = async() => {
-    const users = await prisma.user.findMany({
-        orderBy: { name: 'desc' }
-    });
+const AssingneeSelect = () => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    retry: 3,
+  });
+
+  if (isLoading) return <Skelton height="2rem" />;
+
+  if (error) return null;
 
   return (
     <Select.Root>
-        <Select.Trigger placeholder='Assign ...' />
-        <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                {users.map(user => (
-                    <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
-                ))}
-            </Select.Group>
-        </Select.Content>
+      <Select.Trigger placeholder="Assign ..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default AssingneeSelect
+export default AssingneeSelect;
