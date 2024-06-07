bb9d94ea911dce8fc4465cd37a2c8c1026832191
Sara Komatsu <radwimps.sara@gmail.com>
2024-06-06 19:27:27 -0500

    Show toast notification

--- app/issues/[id]/AssingneeSelect.tsx
+++ app/issues/[id]/AssingneeSelect.tsx
@@ -5,25 +5,23 @@ import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Skelton from "../../components/Skelton";
+import toast, { Toaster } from 'react-hot-toast';
 
 const AssingneeSelect = ({issue}: {issue: Issue}) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useQuery<User[]>({
+      queryKey: ["users"],
+      queryFn: () => axios.get("/api/users").then((res) => res.data),
+      retry: 3,
+    });
 
   const assignedUser = (userId: string) => {
     try{
       axios.patch(`/api/issues/${issue.id}`, {
         assignedToUserId: (userId === "unassigned" ? null : userId) || null
-      });
+      }).catch(() => toast.error("Changes could not be saved."))
     }catch{
-
+      console.log(error);
+      
     }
   }
 
@@ -49,6 +47,7 @@ const AssingneeSelect = ({issue}: {issue: Issue}) => {
           ))}
         </Select.Group>
       </Select.Content>
+      <Toaster />
     </Select.Root>
   );
 };
