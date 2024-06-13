c92c6afcb73ce378242a479605244cdf568188ac
Sara Komatsu <radwimps.sara@gmail.com>
2024-06-13 17:22:42 -0600

    Build the Issue Summary Component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,37 @@
+import { Status } from '@prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import Link from 'next/link';
+import React from 'react'
+
+interface Props {
+    open: number,
+    inProgress: number,
+    closed: number
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+    const containers: { label: string, value: number, status: Status}[] = [
+        {label: "Open Issues", value: open, status: "OPEN"},
+        {label: "In-Progress Issues ", value: inProgress, status: "IN_PROGRESS"},
+        {label: "Closed Issues ", value: closed, status: "CLOSED"}
+    ];
+
+  return (
+    <Flex gap="5">
+        {containers.map(container => (
+            <Link key={container.label} href={`/issues/list?status=${container.status}`}>
+                <Card>
+                    <Flex direction="column" gap="1">
+                        <Text weight="medium">{container.label}</Text>
+                        <Text size="5" weight="bold" className='pl-1'>
+                            {container.value}
+                        </Text>
+                    </Flex>
+                </Card>
+            </Link>
+        ))}
+    </Flex>
+  )
+}
+
+export default IssueSummary
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import Pagination from "./components/Pagination";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
-export default function Home({searchParams}: {searchParams: { page: string }}) {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN"}});
+  const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS"}});
+  const closed = await prisma.issue.count({ where: { status: "CLOSED"}});
+ 
   return(
     <>
-     <LatestIssues />
+    <IssueSummary open={open} inProgress={inProgress} closed={closed}/>
+    <LatestIssues />
     </>
   );
 }
